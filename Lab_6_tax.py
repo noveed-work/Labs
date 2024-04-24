571169a307c01d722f94ca9f1f843fa075725f99
Noveed Muhammed <noveed.muhammed@hotmail.com>
2024-04-25 00:44:53 +0100

    uploading labs 6 with updated variables

--- Lab_6_tax.py
+++ Lab_6_tax.py
@@ -1,15 +1,15 @@
 def getIncomeTax(pa=11850):
     return salary - pa
 
-salary = int(input("Please enter your salary: "))
+salary = int(input("Please enter your salary: £"))
 salary = getIncomeTax()
 
 if salary <= 34500:
-    tax = ((salary * 20) / 100)
-    print(f"The tax that you will pay is £{tax}")
+    income_tax = ((salary * 20) / 100)
+    print(f"The tax that you will pay is £{income_tax}")
 elif salary <= 150000:
-    tax = ((salary * 40) / 100)
-    print(f"The tax that you will pay is £{tax}")
+    income_tax = ((salary * 40) / 100)
+    print(f"The tax that you will pay is £{income_tax}")
 elif salary > 150000:
-    tax = ((salary * 45) / 100)
-    print(f"The tax that you will pay is £{tax}")
+    income_tax = ((salary * 45) / 100)
+    print(f"The tax that you will pay is £{income_tax}")
--- Lab_6_tax_noFunction.py
+++ Lab_6_tax_noFunction.py
@@ -4,11 +4,11 @@ salary = int(input("Enter Salary: "))
 salary = salary - 11850
 
 if salary <= 34500:
-    it = ((salary / 100) * 20)
-    print(f"Your income tax is {it}")
+    income_tax = ((salary / 100) * 20)
+    print(f"Your income tax is {income_tax}")
 elif salary <= 150000:
-    it = ((salary / 100) * 40)
-    print(f"Your income tax is {it}")
+    income_tax = ((salary / 100) * 40)
+    print(f"Your income tax is {income_tax}")
 elif salary> 150000:
-    it = ((salary / 100) * 45)
-    print(f"Your income tax is {it}")
+    income_tax = ((salary / 100) * 45)
+    print(f"Your income tax is {income_tax}")
